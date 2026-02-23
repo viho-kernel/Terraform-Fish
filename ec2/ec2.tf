afe7deda690b07b308b77ac2c515ca29997d6642
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-23 09:32:57 +0530

    latest commit

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,10 +1,43 @@
 resource "aws_instance" "example" {
   ami = "ami-0220d79f3f480ecf5"
+  vpc_security_group_ids = [aws_security_group.security-group1.id]
 
   instance_type = "t3.micro"
   tags = {
-    Name = "test-spot"
+    Name = "terraform"
     Project = "Roboshop"
   }
+
+}
+
+resource "aws_security_group" "security-group1" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   
+  tags = {
+    Name = "Terraform-securtiy-group"
+  }
 }
